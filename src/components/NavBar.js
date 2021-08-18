e8f2b886a0787ee58831374b2b14ffeb3a3306eb
unknown <gorgyash@gmail.com>
2021-08-18 16:41:53 +0545

    .

---
+++ src/components/NavBar.js
@@ -0,0 +1,11 @@
+import { Box } from "@material-ui/core";
+
+function NavBar(){
+    return (
+        <Box sx={{ backgroundColor : "blue", margin: 0}}>
+            NavBar
+        </Box>
+    );
+}
+
+export default NavBar;
