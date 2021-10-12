6bd2522ac165f86526d41e31242af7218f6be7dc
furyash <gorgyash@gmail.com>
2021-10-12 00:36:15 -0700

    added back firebase

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import MainDisplay from "./components/MainDisplay";
 //import { MenuContextProvider } from "./store/menuContext";
 import { OrderContextProvider } from "./store/orderContext";
 import MenuContext from "./store/menuContext";
-import data from "./store/mui-4-data.json"; //Used in place of firebase
+//import data from "./store/mui-4-data.json"; //Used in place of firebase
 
 function App() {
   const menuSet = React.useContext(MenuContext);
@@ -14,24 +14,24 @@ function App() {
   const [isLoading, setIsLoading] = React.useState(true);
 
   React.useEffect(() => {
-    // fetch("https://mui-4-f408f-default-rtdb.firebaseio.com/allItems.json")
-    //   .then((response) => response.json())
-    //   .then((data) => {
-    //     setAllItems(data);
-    //   });
-    // fetch("https://mui-4-f408f-default-rtdb.firebaseio.com/subMenu.json")
-    //   .then((response) => response.json())
-    //   .then((data) => {
-    //     setSubMenu(data);
-    //   });
-    // fetch("https://mui-4-f408f-default-rtdb.firebaseio.com/mainMenu.json")
-    //   .then((response) => response.json())
-    //   .then((data) => {
-    //     setMenu(data);
-    //   });
-    setAllItems(data.allItems);
-    setSubMenu(data.subMenu);
-    setMenu(data.mainMenu);
+    fetch("https://mui-4-f408f-default-rtdb.firebaseio.com/allItems.json")
+      .then((response) => response.json())
+      .then((data) => {
+        setAllItems(data);
+      });
+    fetch("https://mui-4-f408f-default-rtdb.firebaseio.com/subMenu.json")
+      .then((response) => response.json())
+      .then((data) => {
+        setSubMenu(data);
+      });
+    fetch("https://mui-4-f408f-default-rtdb.firebaseio.com/mainMenu.json")
+      .then((response) => response.json())
+      .then((data) => {
+        setMenu(data);
+      });
+    // setAllItems(data.allItems);
+    // setSubMenu(data.subMenu);
+    // setMenu(data.mainMenu);
   }, []);
 
   React.useEffect(() => {
